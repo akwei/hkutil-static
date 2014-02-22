95730f4e1318fef9a5f2ec2fb5e41563f24a0300
akwei <ak478288@gmail.com>
2014-02-23 00:12:24 +0800

    fix bug

--- util/tools/HKMVC.h
+++ util/tools/HKMVC.h
@@ -6,13 +6,52 @@
 //  Copyright (c) 2014年 huoku. All rights reserved.
 //
 
-#import "HKKVO.h"
 
-@interface HKMVC : HKKVO<HKKVODelegate>
+@class HKMVC;
+
+@protocol HKMVCDelegate <NSObject>
+@required
+-(void)mvcOnReceive:(HKMVC*)hkMVC;
+@end
+
+@interface HKMVC : NSObject
 @property(nonatomic,assign)id mvcDelegate;
+@property(nonatomic,copy)NSString* result;
+
++(void)setInjectBlock:(void (^)(void))injectBlock;
 
 -(id)initWithMvcDelegate:(id)mvcDelegate;
+
+
+/**
+ 开启单元测试模式，如果开启，将对程序中的异步都进行同步化处理。默认不开启
+ @param enable YES:开启
+ */
++(void)setEnableTestMode:(BOOL)enable;
+
+/**
+ 是否开启了单元测试模式
+ @returns YES:开启
+ */
++(BOOL)isEnableTestMode;
+
+/**
+ 异步提交到并发队列中
+ @param block 需要异步执行的block，返回一个字符串代表状态
+ */
+-(void)async:(NSString* (^)(void))block;
+
+/**
+ 添加一组异步执行的blok到特定的组中，此方法为阻塞方法，只有执行完所有线程之后，才能继续运行
+ @param blockArray 需要异步执行的block 形式: (NSString* (^)(void))block
+ @param group 线程组
+ */
+-(void)asyncWithBlockArrayToGroup:(NSArray*)blockArray;
+
 -(void)setInfoValue:(id)value forKey:(NSString*)key;
 -(id)infoValueForKey:(NSString *)key;
+-(void)noticeMvc;
+
+
 
 @end
--- util/tools/HKMVC.m
+++ util/tools/HKMVC.m
@@ -9,31 +9,48 @@
 #import "HKMVC.h"
 #import <objc/message.h>
 
+static HKMVC* _sharedObj = nil;
+static BOOL _sharedEnableTestMode = NO;
+
 @interface HKMVC ()
 @property(nonatomic,strong)NSMutableDictionary* info;
+@property(nonatomic,assign)dispatch_queue_t asyncQueue;
 @end
+
+static void (^__injectBlock)(void) = nil;
 
 @implementation HKMVC
 
-- (id)init
-{
-    self = [super init];
-    if (self) {
-        self.hkKVODelegate = self;
-        self.info = [[NSMutableDictionary alloc] init];
-    }
-    return self;
++(void)setInjectBlock:(void (^)(void))injectBlock{
+    __injectBlock = injectBlock;
+}
+
++(void)setEnableTestMode:(BOOL)enable{
+    _sharedEnableTestMode = enable;
+}
+
++(BOOL)isEnableTestMode{
+    return _sharedEnableTestMode;
 }
 
 -(id)initWithMvcDelegate:(id)mvcDelegate{
     self = [super init];
     if (self) {
-        self.hkKVODelegate = self;
+        _asyncQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+        [self addObserver:self forKeyPath:@"result" options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld context:NULL];
         self.info = [[NSMutableDictionary alloc] init];
         self.mvcDelegate = mvcDelegate;
     }
     return self;
 }
+
+-(void)dealloc{
+    [self removeObserver:self forKeyPath:@"result"];
+#if NEEDS_DISPATCH_RETAIN_RELEASE
+	if (_asyncQueue) dispatch_release(_asyncQueue);
+#endif
+}
+
 
 -(void)setInfoValue:(id)value forKey:(NSString *)key{
     [self.info setValue:value forKey:key];
@@ -45,30 +62,51 @@
 
 -(void)async:(NSString *(^)(void))block{
     [self.info removeAllObjects];
-    [super async:block];
+    if ([HKMVC isEnableTestMode]) {
+        self.result = block();
+        return;
+    }
+    __weak HKMVC* me = self;
+    dispatch_async(_asyncQueue, ^{
+        me.result = block();
+    });
 }
 
 -(void)asyncWithBlockArrayToGroup:(NSArray *)blockArray{
     [self.info removeAllObjects];
-    [super asyncWithBlockArrayToGroup:blockArray];
+    if ([HKMVC isEnableTestMode]) {
+        for (NSString* (^block)(void)  in blockArray) {
+            self.result = block();
+        }
+        return;
+    }
+    dispatch_group_t group = dispatch_group_create();
+    __weak HKMVC* me = self;
+    for (NSString* (^block)(void)  in blockArray) {
+        dispatch_group_async(group, _asyncQueue, ^{
+            me.result = block();
+        });
+    }
+    dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
 }
 
 -(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context{
     if ([keyPath isEqualToString:@"result"]) {
-        __weak HKKVO* me = self;
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [me.hkKVODelegate hkKVOOnReceive:me];
-        });
+        __weak HKMVC* me = self;
+        SEL selector = NSSelectorFromString(self.result);
+        if (!selector) {
+            return;
+        }
+        if ([me.mvcDelegate respondsToSelector:selector]) {
+            [me.mvcDelegate performSelectorOnMainThread:selector withObject:nil waitUntilDone:YES];
+        }
+        
     }
 }
 
--(void)hkKVOOnReceive:(HKKVO *)hkKVO{
-    SEL selector = NSSelectorFromString(self.result);
-    if (!selector) {
-        return;
-    }
-    if ([self.mvcDelegate respondsToSelector:selector]) {
-        objc_msgSend(self.mvcDelegate, selector);
+-(void)noticeMvc{
+    if (__injectBlock) {
+        __injectBlock();
     }
 }
 
